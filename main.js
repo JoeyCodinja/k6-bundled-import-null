bed0d70349f02f057d757335f506451f3e9f31cf
Danuel Williams <dwillie390@cable.comcast.com>
2022-08-11 09:40:23 -0500

    Adds cross-fetch as dependency to swap out for k6/http library

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ import http from 'k6/http';
 import { check } from 'k6';
 import cheerio from 'cheerio';
 
+// Inclusion of another http client library to coordinate external requests
+import fetch from 'cross-fetch';
+
 export default function () {
-  const res = http.get('https://loadimpact.com/');
+  console.log("fetch => ", fetch);
+  const res = fetch('https://loadimpact.com')
   const $ = cheerio.load(res.body);
 
   const title = $('head title').text();
